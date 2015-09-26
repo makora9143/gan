99915f8ee01c03e79155cd7f22676c81104bbe69
makora9143 <makoto.kawano@gmail.com>
2015-09-26 15:51:24 +0900

    fix

--- test_gan.py
+++ test_gan.py
@@ -62,7 +62,7 @@ def test_gan(
 
 if __name__ == '__main__':
     data, model = test_gan(
-        n_iters=250,
+        n_iters=120,
         learning_rate=0.1,
         n_mc_samples=1,
         scale_init=1.,
